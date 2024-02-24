090114be6e7622abac6a66677e816bdb64740b2f
felipy <felipy81br@gmail.com>
2024-02-24 20:37:52 -0300

    exemplo de programação imperativa no componente component

--- 1-Fundamentos/2-rede-social/src/components/Comment.jsx
+++ 1-Fundamentos/2-rede-social/src/components/Comment.jsx
@@ -1,30 +1,47 @@
 import {ThumbsUp, Trash} from '@phosphor-icons/react'
+import {format, formatDistanceToNow} from 'date-fns'
+import {ptBR} from 'date-fns/locale'
 
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
 
-export function Comment() {
+export function Comment({author,content,publishedAt, likes}) {
+
+    const publishedAtFormated = format(publishedAt, "do 'de' MMMM y HH:mm'h'", {
+        locale: ptBR
+    })
+    const publishedAtDistanceFormated = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true
+    })
 
     return (
         <div className={styles.comment}>
-            <Avatar hasBorder={false} src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <Avatar hasBorder={false} src={author.avatarUrl} />
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                     <header>
                         <div className={styles.authorAndTime}>
-                            <strong>Jenny Wilson</strong>
-                            <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:38">Cerca de 1h atrás</time>
+                            <strong>{author.name}</strong>
+                            {/* <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:38">Cerca de 1h atrás</time> */}
+                            <time title={publishedAtFormated} dateTime={publishedAt.toISOString()}>{publishedAtDistanceFormated}</time>
                         </div>
                         <button title="deletar comentário">
                             <Trash size={20} />
                         </button>
                     </header>
-                    <p>Adorei seu novo portifa Devon!</p>
+                    {
+                        content.map(line => {
+                            return (
+                                <p>{line}</p>
+                            )
+                        })
+                    }
                 </div>
                 <footer>
                     <button>
                         <ThumbsUp size={24} /> 
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likes}</span>
                     </button>
                 </footer>
             </div>
--- 1-Fundamentos/2-rede-social/src/components/Post.jsx
+++ 1-Fundamentos/2-rede-social/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import {format, formatDistanceToNow} from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 
@@ -8,6 +9,21 @@ import styles from './Post.module.css'
 
 export function Post({author, content, publishedAt}){
 
+        let [comments, setComments] = useState([
+        {
+            author: {
+                name: 'Jenny Wilson',
+                avatarUrl: 'https://plus.unsplash.com/premium_photo-1661716909682-d21f1fadb186?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            },
+            content: [
+                'Adorei seu novo portifa Devon!',
+                'Vou entrar la e seguir'
+            ],
+            likes: 12,
+            publishedAt: new Date('2024-02-23 21:13:38')
+        },
+    ])
+
     // trabalhando com datas de forma nativa no javascript
     // const publishedDateFormatted = new Intl.DateTimeFormat('pt-BR',{
     //     day: '2-digit',
@@ -15,7 +31,6 @@ export function Post({author, content, publishedAt}){
     //     hour: '2-digit',
     //     minute: '2-digit'
     // }).format(publishedAt);
-    console.log(publishedAt)
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'",{
         locale: ptBR
     })
@@ -25,6 +40,39 @@ export function Post({author, content, publishedAt}){
         locale: ptBR,
         addSuffix: true
     })
+
+    /*
+        recomendação de prefixo handle no nome da função
+        essa função é acionada atraves da ação de um usuário o diego coloca o prefixo handle, seja um clique de um botão ou submit de um formulario
+        comportamento padrão de um formualario é redirecionar o usuário para uma pagina, evitar comportamento padrão em SPAs
+        O que é o estado de componentes?
+            variaveis que eu quero ele monitore, se não houvesse isso precisaria acompanhar o html inteiro 
+            eu preciso usar estado para renderizar o componente novamente quando o valor da propriedade mudar
+        imutabilidade:
+            useState utiliza esse conteito
+            eu passo o novo valor inves de passar apenas a alteração
+    */
+    function handleCreateNewComment() {
+        event.preventDefault()
+        console.log('submit')
+        // pegando o valor de um input dentro de um form
+
+        const newComment = {
+            author: {
+                name: 'Alexandra fernandes',
+                avatarUrl: 'https://images.unsplash.com/photo-1537803340330-1dccb2774801?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            },
+            content: [
+                event.target.comment.value
+            ],
+            likes: 30,
+            publishedAt: new Date('2024-02-22 21:13:38')
+        }
+
+        setComments([...comments,{...newComment}])
+
+        event.target.comment.value = ''
+    }
 
     return (
         <article className={styles.post}>
@@ -51,17 +99,26 @@ export function Post({author, content, publishedAt}){
                     return <></>
                 })}
             </div>
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
-                <textarea placeholder="Deixe um comentário" />
+                <textarea name="comment" placeholder="Deixe um comentário" />
                 <footer>
                     <button type="submit">Publicar</button>
                 </footer>
             </form>
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {
+                    comments.map(comment => {
+                        return (
+                        <Comment 
+                            author={comment.author} 
+                            content={comment.content}
+                            publishedAt={comment.publishedAt} 
+                            likes={comment.likes}
+                        />
+                        )
+                    })
+                }
             </div>
         </article>
     )
