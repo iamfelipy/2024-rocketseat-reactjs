bee8a140182bc61366da7922d9da4a64f3dc4c15
felipy <felipy81br@gmail.com>
2024-03-07 16:36:05 -0300

    -criando arquivo *.d.ts de tipagem extra -criando globalStyle styled-component -criandos variaveis globais css -configuração do eslint + preset rockeseat

--- 2-aprofundando-em-hooks/src/App.tsx
+++ 2-aprofundando-em-hooks/src/App.tsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
 import './App.css'
 import { Button } from './components/Button'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
+      <ThemeProvider theme={defaultTheme}>
         <Button variant="primary" />
         <Button variant="secondary" />
         <Button variant="sucess" />
         <Button variant="danger" />
         <Button />
-      </div>
+        <GlobalStyle />
+      </ThemeProvider>
     </>
   )
 }
-
