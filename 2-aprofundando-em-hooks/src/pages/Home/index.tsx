29575ec12ed1beca391f020f1fedea7399e67dc7
felipy <felipy81br@gmail.com>
2024-03-11 08:29:58 -0300

    adicionado: useForm -> formulario react zod -> validação formulário compartilhando interface do zod para useForm

--- 2-aprofundando-em-hooks/src/pages/Home/index.tsx
+++ 2-aprofundando-em-hooks/src/pages/Home/index.tsx
@@ -8,18 +8,75 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './styles'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no minimo 5 minutos.')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
+  owner: zod.string().optional(),
+})
+
+// definido manualmentee
+// jeito 1
+// interface NewCycleFormData {
+//   task: string
+//   minutesAmount: number
+// }
+
+// automatizando a tipagem
+// jeito 2
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
+  const {
+    // definir campos do formulario
+    register,
+    // enviar formulario
+    handleSubmit,
+    // pegar o valor dos inputs definidos com o register
+    watch,
+    // capturar o erros do formulário
+    formState,
+    // função para resetar o fomulário depois de enviado
+    reset,
+  } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    // inicio o formulario com esses valores
+    // deixa esses valores na hora de usar a função reset
+    defaultValues: {
+      task: 'teste',
+      minutesAmount: 123,
+    },
+  })
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(data)
+    reset()
+  }
+
+  // testando a captura de erro com zod + useForm
+  // console.log(formState.errors)
+
+  // propriedade watch, transform o componente em controlled e faz a reenderização toda vez que o valor da variavel mudar
+  const task = watch('task')
+  const isSubmitDisable = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
-            type="text"
             id="task"
-            placeholder="De um nome para a sua tarefa"
+            type="text"
             list="task-suggestions"
+            placeholder="De um nome para a sua tarefa"
+            {...register('task')}
           />
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmount
@@ -29,6 +86,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
           <datalist id="task-suggestions">
             <option value="Projeto 1"></option>
@@ -46,7 +104,7 @@ export function Home() {
           <span>0</span>
           <span>0</span>
         </CountdownContainer>
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={isSubmitDisable} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
