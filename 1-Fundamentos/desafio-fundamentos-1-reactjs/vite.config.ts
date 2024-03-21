8ca47c69e3c5b5f7169a8fc544c1f01af80b5092
felipy <felipy81br@gmail.com>
2024-03-21 15:25:51 -0300

    configuração git hub pages, build vite

--- 1-Fundamentos/desafio-fundamentos-1-reactjs/vite.config.ts
+++ 1-Fundamentos/desafio-fundamentos-1-reactjs/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/2024-rocketseat-reactjs/1-Fundamentos/desafio-fundamentos-1-reactjs/dist',
 })
