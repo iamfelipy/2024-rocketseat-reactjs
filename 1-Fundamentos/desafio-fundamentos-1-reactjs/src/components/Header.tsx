b4e13427f6801ea57c6e7dd9a00a1ae164026b1a
felipy <felipy81br@gmail.com>
2024-03-21 15:34:23 -0300

    configuração: vite build git hub pages

--- 1-Fundamentos/desafio-fundamentos-1-reactjs/src/components/Header.tsx
+++ 1-Fundamentos/desafio-fundamentos-1-reactjs/src/components/Header.tsx
@@ -1,9 +1,10 @@
 import styles from './Header.module.css'
+import logoToDo from '../assets/logo.svg'
 
 export function Header() {
   return (
     <header className={styles.container}>
-      <img src="/logo.svg" alt="logo da aplicação" />
+      <img src={logoToDo} alt="logo da aplicação" />
     </header>
   )
 }
