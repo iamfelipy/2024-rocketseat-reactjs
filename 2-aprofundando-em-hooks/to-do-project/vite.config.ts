dccbd1ca528a34a0cebcc4fa1066cccabee337a1
felipy <felipy81br@gmail.com>
2024-03-21 22:42:46 -0300

    configuração de build to-do pomodoro reactjs

--- 2-aprofundando-em-hooks/to-do-project/vite.config.ts
+++ 2-aprofundando-em-hooks/to-do-project/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/2024-rocketseat-reactjs/2-aprofundando-em-hooks/todo/dist',
+  base: '/2024-rocketseat-reactjs/2-aprofundando-em-hooks/to-do-pomodoro/dist',
 })
