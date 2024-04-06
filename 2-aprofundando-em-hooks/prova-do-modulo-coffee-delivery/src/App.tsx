75690f6b4655e1157e693e888d635dd4ee2dde92
felipy <felipy81br@gmail.com>
2024-04-05 21:16:03 -0300

    configuração para o react router dom

--- 2-aprofundando-em-hooks/prova-do-modulo-coffee-delivery/src/App.tsx
+++ 2-aprofundando-em-hooks/prova-do-modulo-coffee-delivery/src/App.tsx
@@ -1,14 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { GlobalStyle } from './styles/global'
-import { defaultTheme } from './styles/themes/default'
+import { Outlet } from 'react-router-dom'
 
-function App() {
+import { Header } from './components/Header'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
+export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <p>teste</p>
       <GlobalStyle />
+      <Header />
+      <Outlet />
     </ThemeProvider>
   )
 }
-
-export default App
--- 2-aprofundando-em-hooks/prova-do-modulo-coffee-delivery/src/main.tsx
+++ 2-aprofundando-em-hooks/prova-do-modulo-coffee-delivery/src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { App } from './App.tsx'
+import { Home } from './pages/Home/index.tsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
