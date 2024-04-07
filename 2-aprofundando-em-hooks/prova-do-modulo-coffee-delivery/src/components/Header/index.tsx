abce60b371cde282e807b764d47beacbf4e53772
felipy <felipy81br@gmail.com>
2024-04-07 08:54:05 -0300

    componente header

--- 2-aprofundando-em-hooks/prova-do-modulo-coffee-delivery/src/components/Header/index.tsx
+++ 2-aprofundando-em-hooks/prova-do-modulo-coffee-delivery/src/components/Header/index.tsx
@@ -1,5 +1,26 @@
-import { Container } from './styles'
+import { Link } from 'react-router-dom'
+import { Aside, Container } from './styles'
+import { MapPin, ShoppingCart } from '@phosphor-icons/react'
 
 export function Header() {
-  return <Container>Header</Container>
+  const cart = []
+
+  return (
+    <Container>
+      <Link to="/">
+        <img src="/logo.svg" alt="Coffee Delivery" />
+      </Link>
+      <Aside>
+        <div>
+          <MapPin size={22} weight="fill" />
+          <span>Porto Alegre, RS</span>
+        </div>
+
+        <Link to={`cart`} aria-disabled={cart.length === 0}>
+          <ShoppingCart size={22} weight="fill" />
+          {cart.length > 0 ? <span>{cart.length}</span> : null}
+        </Link>
+      </Aside>
+    </Container>
+  )
 }
