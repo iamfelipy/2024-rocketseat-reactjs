3e1193b075445f9e155db2dd5f15fc973b8598dc
felipy <felipy81br@gmail.com>
2024-04-16 10:36:14 -0300

    calculando resumo das transações

--- 3-http-performance/dt-money/src/components/Summary/index.tsx
+++ 3-http-performance/dt-money/src/components/Summary/index.tsx
@@ -7,27 +7,43 @@ export function Summary(){
 
   const {transactions} = useContext(TransactionContext)
 
+  const summary = transactions.reduce((acc, transaction) => {
+    if(transaction.type === 'income') {
+      acc.income += transaction.price
+      acc.total += transaction.price
+    } else {
+      acc.outcome += transaction.price
+      acc.total -= transaction.price
+    }
+
+    return acc
+  }, {
+    income: 0,
+    outcome: 0,
+    total: 0
+  })
+
   return <SummaryContainer>
     <SummaryCard>
       <header>
         <span>Entradas</span>
         <ArrowCircleUp size={32} color="#00b37e"/>
       </header>
-      <strong>R$ 17.400,00</strong>
+      <strong>R$ {summary.income}</strong>
     </SummaryCard>
     <SummaryCard>
       <header>
         <span>Saídas</span>
         <ArrowCircleDown size={32} color="#f75a68"/>
       </header>
-      <strong>R$ 17.400,00</strong>
+      <strong>R$ {summary.outcome}</strong>
     </SummaryCard>
     <SummaryCard variant="green">
       <header>
         <span>Total</span>
         <CurrencyDollar size={32} color="#fff"/>
       </header>
-      <strong>R$ 17.400,00</strong>
+      <strong>R$ {summary.total}</strong>
     </SummaryCard>
   </SummaryContainer>
 }
