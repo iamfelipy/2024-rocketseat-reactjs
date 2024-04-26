57332fa1fb5c02dcb5e752354a5aa3df65533286
felipy <felipy81br@gmail.com>
2024-04-26 19:18:56 -0300

    feat: 1-configurar styled-components 2-adicionar theme com as cores do figma 3-criar mixins 4-criar estilo global

--- 3-http-performance/prova-do-modulo-github-blog/src/App.tsx
+++ 3-http-performance/prova-do-modulo-github-blog/src/App.tsx
@@ -1,8 +1,13 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <div>GIthub Blog</div>
-    </>
+    </ThemeProvider>
   )
 }
 
