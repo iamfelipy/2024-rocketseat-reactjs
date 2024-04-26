75c7842f4c54887c34db5d36627c6294589ee76f
felipy <felipy81br@gmail.com>
2024-04-26 19:27:19 -0300

    feat: Configuração do react-router-dom

--- 3-http-performance/prova-do-modulo-github-blog/src/App.tsx
+++ 3-http-performance/prova-do-modulo-github-blog/src/App.tsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Outlet } from 'react-router'
+import { Header } from './components/Header/Header'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <div>GIthub Blog</div>
+      <Header />
+      <Outlet />
     </ThemeProvider>
   )
 }
--- 3-http-performance/prova-do-modulo-github-blog/src/main.tsx
+++ 3-http-performance/prova-do-modulo-github-blog/src/main.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
+import { Home } from './pages/Home/index.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
