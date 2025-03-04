451498336ffed8c749c33ce5dadb5c9b0b67b2d2
felipy <felipy81br@gmail.com>
2025-03-04 10:35:30 -0300

    test: add check for active navlink highlight

---
+++ 4-Integrando-frontend-backend/src/components/nav-link.spec.tsx
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+
+import { NavLink } from './nav-link'
+
+describe('NavLink', () => {
+  it('should highlight the nav link when is the current page link', () => {
+    // alterar o estado da url
+    // render componente
+    // procurar pelo elemento
+    // verificar se tem o data-attibute type true
+
+    const wrapper = render(
+      <>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </>,
+      {
+        wrapper: ({ children }) => {
+          return (
+            <MemoryRouter initialEntries={['/about']}>{children}</MemoryRouter>
+          )
+        },
+      },
+    )
+    expect(wrapper.getByText('Home').dataset.current).toEqual('false')
+    expect(wrapper.getByText('About').dataset.current).toEqual('true')
+  })
+})
