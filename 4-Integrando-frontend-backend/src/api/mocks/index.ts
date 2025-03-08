836fe122709c2cce5ed727898a2d25274151ad6a
felipy <felipy81br@gmail.com>
2025-03-08 14:27:47 -0300

    feat(mocks): add some mocks

--- 4-Integrando-frontend-backend/src/api/mocks/index.ts
+++ 4-Integrando-frontend-backend/src/api/mocks/index.ts
@@ -8,6 +8,8 @@ import { getManagedRestaurantMock } from './get-managed-restaurant-mock'
 import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
 import { getMonthOrdersAmountMock } from './get-month-orders-amount-mock'
 import { getMonthRevenueAmountMock } from './get-month-revenue-mock'
+import { getOrdersDetailsMock } from './get-order-details-mock'
+import { getOrdersMock } from './get-orders-mock'
 import { getPopularProductsMock } from './get-popular-products-mock'
 import { geProfileMock } from './get-profile-mock'
 import { registerRestaurantMock } from './register-restaurant-mock'
@@ -26,6 +28,8 @@ export const worker = setupWorker(
   geProfileMock,
   getManagedRestaurantMock,
   updateProfileMock,
+  getOrdersMock,
+  getOrdersDetailsMock,
 )
 
 export async function enableMSW() {
