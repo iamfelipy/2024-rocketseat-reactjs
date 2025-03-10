312ef33950c2c193e0a09d605df5e91bec4a27b3
felipy <felipy81br@gmail.com>
2025-03-10 10:03:49 -0300

    chore(e2e): config playwright

--- 4-Integrando-frontend-backend/playwright.config.ts
+++ 4-Integrando-frontend-backend/playwright.config.ts
@@ -12,6 +12,7 @@ import { devices } from '@playwright/test'
  */
 const config: PlaywrightTestConfig = {
   testDir: './test',
+  testMatch: /.*\.e2e-spec\.ts$/,
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
@@ -30,78 +31,79 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:50789',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    // trace: 'on-first-retry',
+  },
+
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: 'npm run dev:test',
+    url: 'http://localhost:50789',
+    reuseExistingServer: !process.env.CI,
   },
 
   /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-      },
-    },
+  // projects: [
+  //   {
+  //     name: 'chromium',
+  //     use: {
+  //       ...devices['Desktop Chrome'],
+  //     },
+  //   },
 
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-      },
-    },
+  //   {
+  //     name: 'firefox',
+  //     use: {
+  //       ...devices['Desktop Firefox'],
+  //     },
+  //   },
 
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },
+  //   {
+  //     name: 'webkit',
+  //     use: {
+  //       ...devices['Desktop Safari'],
+  //     },
+  //   },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: {
-    //     ...devices['iPhone 12'],
-    //   },
-    // },
+  //   /* Test against mobile viewports. */
+  //   // {
+  //   //   name: 'Mobile Chrome',
+  //   //   use: {
+  //   //     ...devices['Pixel 5'],
+  //   //   },
+  //   // },
+  //   // {
+  //   //   name: 'Mobile Safari',
+  //   //   use: {
+  //   //     ...devices['iPhone 12'],
+  //   //   },
+  //   // },
 
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: {
-    //     channel: 'msedge',
-    //   },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: {
-    //     channel: 'chrome',
-    //   },
-    // },
-  ],
+  //   /* Test against branded browsers. */
+  //   // {
+  //   //   name: 'Microsoft Edge',
+  //   //   use: {
+  //   //     channel: 'msedge',
+  //   //   },
+  //   // },
+  //   // {
+  //   //   name: 'Google Chrome',
+  //   //   use: {
+  //   //     channel: 'chrome',
+  //   //   },
+  //   // },
+  // ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   // outputDir: 'test-results/',
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
 }
 
 export default config
