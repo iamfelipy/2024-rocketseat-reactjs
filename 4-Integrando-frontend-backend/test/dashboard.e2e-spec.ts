284cc6856547dfc396959edd7f2c34114613bea3
felipy <felipy81br@gmail.com>
2025-03-11 09:53:56 -0300

    test(e2e): add dashboard metrics

---
+++ 4-Integrando-frontend-backend/test/dashboard.e2e-spec.ts
@@ -0,0 +1,30 @@
+import { expect, test } from '@playwright/test'
+
+test('display day orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+  expect(page.getByText('20', { exact: true })).toBeVisible()
+  expect(
+    page.getByText('-5% em relação a ontem', { exact: true }),
+  ).toBeVisible()
+})
+test('display month orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+  expect(page.getByText('200', { exact: true })).toBeVisible()
+  expect(
+    page.getByText('+20% relação ao mês passado', { exact: true }),
+  ).toBeVisible()
+})
+test('display month canceled orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+  expect(page.getByText('50', { exact: true })).toBeVisible()
+  expect(
+    page.getByText('+10% relação ao mês passado', { exact: true }),
+  ).toBeVisible()
+})
+test('display month revenue metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+  expect(page.getByText('200', { exact: true })).toBeVisible()
+  expect(
+    page.getByText('+40 relação ao mês passado', { exact: true }),
+  ).toBeVisible()
+})
