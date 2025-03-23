6e391a8182c007d8a08426a05d309be7d7322669
felipy <felipy81br@gmail.com>
2025-03-23 20:33:28 -0300

    feat: add product images and implement home page layout with css-in-js components

--- 5-primeiro-framework/src/pages/index.tsx
+++ 5-primeiro-framework/src/pages/index.tsx
@@ -1,23 +1,20 @@
-import {styled} from '../styles'
+import Image from 'next/future/image'
+import { HomeContainer, Product } from '../styles/pages/home'
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-  span: {
-    fontWeight: 'bold'
-  },
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
 
 export default function Home() {
   return (
-    <Button>
-      <span>Test</span>
-      Enviar
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
