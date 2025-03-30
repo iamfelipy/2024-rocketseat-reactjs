5045a969e7c17397d79e561ec2996213a1a71319
felipy <felipy81br@gmail.com>
2025-03-30 17:36:56 -0300

    feat: create badge number info quantity of the cart

--- 5-primeiro-framework/desafio-do-modulo-ignite-shop-v2/src/components/Header.tsx
+++ 5-primeiro-framework/desafio-do-modulo-ignite-shop-v2/src/components/Header.tsx
@@ -1,19 +1,20 @@
-import { Handbag } from "@phosphor-icons/react/dist/ssr"
+import { Handbag } from "@phosphor-icons/react"
 import { ShoppingCart } from "./ShoppingCart"
 import logoImg from '../assets/logo.svg'
 import Image from 'next/future/image'
 import { useShoppingCart } from "use-shopping-cart"
-import {Container} from '../styles/components/Header'
+import { Container, CartButtonContainer, CartCountBadge } from '../styles/components/header'
 
 export function Header() {
   const { handleCartClick, cartCount } = useShoppingCart();
   
   return (
     <Container>
-      <Image src={logoImg} width="130" height="52" alt=""/>
-      <button onClick={(e) => handleCartClick()}>
-        <Handbag size="2rem" weight="bold" />
-      </button>
+      <Image src={logoImg} width="130" height="52" alt="Logo" />
+      <CartButtonContainer onClick={handleCartClick}>
+        <Handbag size="1.5rem" weight="bold" />
+        {cartCount > 0 && <CartCountBadge>{cartCount}</CartCountBadge>} {/* Exibe o número de itens */}
+      </CartButtonContainer>
       <ShoppingCart />
     </Container>
   )
--- 5-primeiro-framework/desafio-do-modulo-ignite-shop-v2/src/styles/components/header.ts
+++ 5-primeiro-framework/desafio-do-modulo-ignite-shop-v2/src/styles/components/header.ts
@@ -8,14 +8,40 @@ export const Container = styled('header', {
   width: '100%',
   maxWidth: 1180,
   margin: '0 auto',
+})
 
-  button: {
-    width: '3.5rem',
-    height: '3.5rem',
-    borderRadius: '6px',
-    border: 'none',
-    color: '$gray600',
-    backgroundColor: '$gray800',
-    cursor: 'pointer'
-  }
+export const CartButtonContainer = styled('button', {
+  position: 'relative', // Necessário para posicionar o contador de itens
+  width: '3rem',
+  height: '3rem',
+  borderRadius: '6px',
+  border: 'none',
+  color: '$gray600',
+  backgroundColor: '$gray800',
+  cursor: 'pointer',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+
+  '&:hover': {
+    backgroundColor: '$gray700',
+  },
+})
+
+export const CartCountBadge = styled('span', {
+  position: 'absolute',
+  top: '-10px',
+  right: '-10px',
+  backgroundColor: '$green500',
+  color: '#fff',
+  border: '2px solid $gray900',
+  borderRadius: '50%',
+  width: '1.5rem',
+  height: '1.5rem',
+  fontSize: '12px',
+  fontWeight: 'bold',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  boxSizing: 'content-box'
 })
