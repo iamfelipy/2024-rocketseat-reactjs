af6d96564e5b98ae1b9d1b9c919dd54b30d3b11d
felipy <felipy81br@gmail.com>
2025-04-06 23:26:18 -0300

    feat: Add Box component and remove Button component

---
+++ 6-design-system/projeto-do-modulo-design-system/packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,17 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, BoxProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Surfaces/Box',
+  component: Box,
+  // se args é definido aqui ele é compartilhado com todos os stories
+  args: {
+    children: (
+      <>
+        <span>Testando o elemento Box</span>
+      </>
+    ),
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
