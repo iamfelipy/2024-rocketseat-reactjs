85e41207fe7b07c3ca44573e81f76cfed4b08062
felipy <felipy81br@gmail.com>
2025-04-08 09:44:00 -0300

    feat: add Button component with variants and Storybook stories

---
+++ 6-design-system/projeto-do-modulo-design-system/packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,46 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  // se args é definido aqui ele é compartilhado com todos os stories
+  args: {
+    children: 'Send',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secondary',
+    children: 'Create new',
+  },
+}
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancel',
+  },
+}
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+  },
+}
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Próximo passo
+        <ArrowRight weight="bold" />
+      </>
+    ),
+  },
+}
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true,
+  },
+}
