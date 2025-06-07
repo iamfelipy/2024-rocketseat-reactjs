6325bb0cdc3e5e254e62abc77cda9cd1aa63e419
felipy <felipy81br@gmail.com>
2025-06-07 20:03:31 -0300

    chore: move global css styles from pages to src

--- 7-aprofundando-em-nextjs/src/pages/_app.page.tsx
+++ 7-aprofundando-em-nextjs/src/pages/_app.page.tsx
@@ -2,7 +2,7 @@ import '../lib/dayjs'
 
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
-import { globalStyles } from './styles/global'
+import { globalStyles } from '../styles/global'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '../lib/react-query'
 import { DefaultSeo } from 'next-seo'
