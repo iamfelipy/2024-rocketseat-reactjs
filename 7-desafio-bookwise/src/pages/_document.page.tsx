1233d0c1c89d46cdfd3f727c1fcc0dbaf09f2b10
felipy <felipy81br@gmail.com>
2025-06-07 16:55:26 -0300

    feat: add nunito fonts

--- 7-desafio-bookwise/src/pages/_document.page.tsx
+++ 7-desafio-bookwise/src/pages/_document.page.tsx
@@ -5,6 +5,16 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
         <style
           id="stitches"
           dangerouslySetInnerHTML={{ __html: getCssText() }}
