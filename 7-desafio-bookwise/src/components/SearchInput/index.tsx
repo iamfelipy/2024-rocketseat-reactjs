e6e4731ad3e60eba69a6705d0ac93feb0ebe714b
felipy <felipy81br@gmail.com>
2025-06-22 20:25:15 -0300

    feat: integrate React Query and Axios for data fetching in ProfilePage, enhance SearchInput with debounced search functionality

--- 7-desafio-bookwise/src/components/SearchInput/index.tsx
+++ 7-desafio-bookwise/src/components/SearchInput/index.tsx
@@ -1,16 +1,19 @@
-import { InputHTMLAttributes } from 'react'
+import { InputHTMLAttributes, forwardRef, ForwardedRef } from 'react'
 import { MagnifyingGlass } from 'phosphor-react'
 import { IconContainer, Input, SearchInputContainer } from './styles'
 
 type SearchInputProps = InputHTMLAttributes<HTMLInputElement>
 
-export function SearchInput({ ...props }: SearchInputProps) {
+export const SearchInput = forwardRef(function SearchInput(
+  { ...props }: SearchInputProps,
+  ref: ForwardedRef<HTMLInputElement>,
+) {
   return (
     <SearchInputContainer>
-      <Input placeholder="Buscar livro ou autor" {...props} />
+      <Input placeholder="Buscar livro ou autor" ref={ref} {...props} />
       <IconContainer>
         <MagnifyingGlass size={20} />
       </IconContainer>
     </SearchInputContainer>
   )
-}
+})
--- 7-desafio-bookwise/src/pages/_app.page.tsx
+++ 7-desafio-bookwise/src/pages/_app.page.tsx
@@ -2,11 +2,16 @@ import '../lib/dayjs'
 
 import type { AppProps } from 'next/app'
 import { globalStyles } from '@/styles/global'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '../lib/react-query'
 
 globalStyles()
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
-
 export default MyApp
